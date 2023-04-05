c64864f92545e51a0d03a65dd1e2e3b9b8878c58
zamri71 <129921806+zamri71@users.noreply.github.com>
2023-04-05 11:59:54 +0800

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,5 +1,5 @@
 
-/*Do not modify {// [[*... // ]]*} pattern manually*/
+
 
 // [[Gobal Code
 (function(){
@@ -9,32 +9,22 @@
 }());
 // ]]Gobal Code
 
-
 // [[Page Appearance
 xui.ini.$PageAppearance = {
-//    "theme": "moonify",
-//     "background":{
-//       "background-color": "#FFFFFF",
-//       "background-image": "",
-//       "background-repeat": "",
-//       "background-position": "",
-//       "background-attachment": ""
-//    }
+    "theme" : "default"
 };
 // ]]Page Appearance
-
 // To set Font Icons CDN
+
 // [[Font Icons CDN
-xui.ini.$FontIconsCDN ={
-    "fontawesome":{
-        "href":"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-        "integrity":"sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN",
-        "crossorigin":"anonymous",
-        "disabled":true
+xui.ini.$FontIconsCDN = {
+    "fontawesome" : {
+        "href" : "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+        "integrity" : "sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN",
+        "disabled" : true
     }
 };
 // ]]Font Icons CDN
-
 // To set default prop to all xui.UI instances
 // [[Default Prop
 xui.ini.$DefaultProp={
@@ -47,64 +37,63 @@ xui.ini.$DefaultProp={
 
 // [[Global Functions
 xui.$cache.functions = {
-    "$APICaller:beforeData":{
-        "desc":"exception handler",
-        "params":[
+    "$APICaller:beforeData" : {
+        "desc" : "exception handler",
+        "params" : [
             {
-                "id":"rspData",
-                "type":"Hash",
-                "desc":""
+                "id" : "rspData",
+                "type" : "Hash",
+                "desc" : ""
             }
         ],
-        "actions":[
+        "actions" : [
             {
-                "desc":"if returns exception",
-                "type":"other",
-                "target":"msg",
-                "args":[
+                "desc" : "if returns exception",
+                "type" : "other",
+                "target" : "msg",
+                "args" : [
                     "API returns exception",
                     "{args[0].error.message}"
                 ],
-                "method":"alert",
-                "conditions":[
+                "method" : "alert",
+                "conditions" : [
                     {
-                        "left":"{args[0].error}",
-                        "symbol":"non-empty",
-                        "right":""
+                        "left" : "{args[0].error}",
+                        "symbol" : "non-empty",
+                        "right" : ""
                     }
                 ],
-                "timeout":0,
-                "onOK":2
+                "timeout" : 0,
+                "onOK" : 2
             }
         ]
     },
-    "$APICaller:onError":{
-        "desc":"error handler",
-        "params":[
+    "$APICaller:onError" : {
+        "desc" : "error handler",
+        "params" : [
             {
-                "id":"rspData",
-                "type":"Hash",
-                "desc":""
+                "id" : "rspData",
+                "type" : "Hash",
+                "desc" : ""
             }
         ],
-        "actions":[
+        "actions" : [
             {
-                "desc":"if API raises error",
-                "type":"other",
-                "target":"msg",
-                "args":[
+                "desc" : "if API raises error",
+                "type" : "other",
+                "target" : "msg",
+                "args" : [
                     "API raises error",
                     "{args[0]}"
                 ],
-                "method":"alert",
-                "onOK":2,
-                "timeout":0
+                "method" : "alert",
+                "onOK" : 2,
+                "timeout" : 0
             }
         ]
     }
 };
 // ]]Global Functions
-
 // [[Global Data
 xui.$cache.data = {
     "$DATE_FORMAT":"gmt"
@@ -116,3 +105,14 @@ xui.$cache.data = {
 xui.constant = {
 };
 // ]]Constant Data
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf" : {
+        "touchDevice" : true,
+        "width" : 800,
+        "height" : 1280
+    }
+};
+// ]]Develop Env Setting
